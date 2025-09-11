4c68264aaa5b489aa812ac11f2597d1ab11f6535
bmaranan75 <41923066+bmaranan75@users.noreply.github.com>
2025-09-11 15:34:14 -0700

    Change 'errors' key to 'error' in response object

--- src/app/api/inlinehook/route.ts
+++ src/app/api/inlinehook/route.ts
@@ -56,7 +56,7 @@ export async function POST(request: NextRequest) {
     // };
 
     const response = {
-       "errors":[
+       "error":[
           {
              "errorSummary":"Failed to deliver SMS OTP to test.user@okta.com",
              "errorCauses":{
